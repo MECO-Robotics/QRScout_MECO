b34055f6594112600d43cb3962e82ecc4d93b345
Brian Lee <50838034+happyknightII@users.noreply.github.com>
2026-04-23 21:00:04 -0400

    fix github pages entrypoint

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/QRScout_MECO/',
+    base: './',
+    build: {
+        manifest: true,
+    },
     plugins: [
         react(),
         VitePWA({
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout_MECO/',
+  base: './',
+  build: {
+    manifest: true,
+  },
   plugins: [
     react(),
     VitePWA({
